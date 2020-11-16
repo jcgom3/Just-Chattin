6f60468fa8d2f6608fd99f08536db8c3ec80d29e
ienglehart <ienglehart13@gmail.com>
2020-11-16 18:07:52 -0500

    Moved some files
    -

--- models/messages.js
+++ models/messages.js
@@ -2,20 +2,9 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// create our messages model
-class Messages extends Model {
+class Messages extends Model {}
 
-    /* do i need this for room model?
-
-  // set up method to run on instance data (per user) to check password
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-  */
-
-}
-
-// create fields/columns for User model
+// create fields/columns for Messages model
 Messages.init(
   {
     id: {
--- models/rooms.js
+++ models/rooms.js
@@ -2,20 +2,9 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// create our Room model
-class Room extends Model {
+class Room extends Model {}
 
-    /* do i need this for room model?
-
-  // set up method to run on instance data (per user) to check password
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-  */
-
-}
-
-// create fields/columns for User model
+// create fields/columns for Room model
 Room.init(
   {
     id: {
